f30907bf9220f3928f88b112698ae0bd5f1d01c5
99100043 <raedmiranda>
2022-05-22 12:08:10 -0500

    Agregar archivos de proyecto.

---
+++ ECommerce.Auth/Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ECommerce.Auth.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public int Intentos { get; set; }
+        public int NivelSeg { get; set; }
+        public DateTime FechaCracion { get; set; }
+    }
+}
